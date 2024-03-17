a11b1ebeaa585b7ff038ba76670e8b078f1a2d1e
justin <justinpop24@gmail.com>
2024-03-16 22:05:07 -0500

    Have a create  post page

---
+++ Blog/Repository/PeopleRespository.cs
@@ -0,0 +1,46 @@
+﻿using Blog.Data;
+using Blog.Interfaces;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Repository
+{
+    public class PeopleRespository : IPeopleRepository
+
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PeopleRespository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public bool Add(People people)
+        {
+            _context.Add(people);
+            return Save();
+        }
+
+        public bool Delete(People people)
+        {
+            _context.Remove(people);
+            return Save();
+        }
+
+
+        public async Task<People> GetByIdAsync(int id)
+        {
+            return await _context.People.FirstOrDefaultAsync(i => i.PeopleId == id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(People people)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Blog/Repository/PostRespository.cs
@@ -0,0 +1,50 @@
+﻿using Blog.Data;
+using Blog.Interfaces;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Repository
+{
+    public class PostRespository : IPostRespository
+
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostRespository(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+        public bool Add(Post post)
+        {
+            _context.Add(post);
+            return Save();
+        }
+
+        public bool Delete(Post post)
+        {
+            _context.Remove(post);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Post>> GetAll()
+        {
+            return await _context.Posts.Include(a => a.People).ToListAsync();
+        }
+
+        public async Task<Post> GetByIdAsync(int id)
+        {
+            return await _context.Posts.Include(a => a.People).FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(Post post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
